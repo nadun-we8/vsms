c78ea7dcb1ff23a5a4bb8a81615f6e2e6683833b
nadun-we8 <nadun.senarathne@we8.io>
2025-02-05 12:12:01 +0530

    .

--- supplier/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import supplier_list, supplier_detail, supplier_category_list, supplier_category_detail
-
-urlpatterns = [
-    path('supplier/', supplier_list, name='supplier_list'),
-    path('supplier/<int:pk>/', supplier_detail, name='supplier_detail'),
-    path('supplier-category/', supplier_category_list, name='supplier_category_list'),
-    path('supplier-category/<int:pk>/', supplier_category_detail, name='supplier_category_detail'),
-]
